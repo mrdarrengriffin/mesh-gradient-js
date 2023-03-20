db027c74a26f81caed5f33a9cbdedbfb56f62c62
Darren Griffin <darren.griffin@live.co.uk>
2023-03-20 02:14:51 +0000

    WIP: Alpha colours

--- script.js
+++ script.js
@@ -30,6 +30,12 @@ function RGBtoHex(r, g, b) {
     return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
 }
 
+// rgb with alpha to hex
+function RGBAtoHex(r, g, b, a) {
+    return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1) + (1 << 8 | a).toString(16).slice(1);
+}
+
+
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
